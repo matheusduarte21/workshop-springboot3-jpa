f7aae0854ec5a59cb2899c4b5adf16ab471aec24
Matheus Duarte <122234782+matheusduarte21@users.noreply.github.com>
2023-07-17 20:30:53 -0300

    h2 database, test profile, jpa

--- src/main/java/com/educandoweb/course/resources/UserResource.java
+++ src/main/java/com/educandoweb/course/resources/UserResource.java
@@ -12,7 +12,7 @@ import com.educandoweb.course.entities.User;
 public class UserResource {
 	
 	@GetMapping
-	public ResponseEntity<User>findAll(){
+	public ResponseEntity<User>findAll(){ //para retorna respostas de requisições web
 		User u = new User(1L, "Maria", "maria@gmail.com", "93 9999999","12345");
 		return ResponseEntity.ok().body(u);
 	}
