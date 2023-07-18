d7c3b851016b09638bdcc3245f395a1254ffd61f
Matheus Duarte <122234782+matheusduarte21@users.noreply.github.com>
2023-07-18 19:02:51 -0300

    Order, Istant, ISO 8601

--- src/main/java/com/educandoweb/course/entities/User.java
+++ src/main/java/com/educandoweb/course/entities/User.java
@@ -1,12 +1,15 @@
 package com.educandoweb.course.entities;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 
 @Entity
@@ -22,9 +25,12 @@ public class User implements Serializable {
 	private String phone;
 	private String password;
 	
+	@OneToMany(mappedBy = "Client") // mapeado pelo atributo "client"
+	private List<Order> orders = new ArrayList<>();	
+	
 	public User() {
 		
-	}
+	 }
 
 	public User(Long id, String name, String email, String phone, String password) {
 		super();
@@ -90,6 +96,10 @@ public class User implements Serializable {
 			return false;
 		User other = (User) obj;
 		return Objects.equals(id, other.id);
+	}
+
+	public List<Order> getOrders() {
+		return orders;
 	}
 	
 	
