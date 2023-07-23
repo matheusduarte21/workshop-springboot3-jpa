b1e4258b4d4ec60e3db9aa7721e2f4f1d163ccfa
Matheus Duarte <122234782+matheusduarte21@users.noreply.github.com>
2023-07-23 10:06:34 -0300

    Exception handlling - findById

---
+++ src/main/java/com/educandoweb/course/resources/exceptions/ResourceExcepetionHandler.java
@@ -0,0 +1,24 @@
+package com.educandoweb.course.resources.exceptions;
+
+import java.time.Instant;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import com.educandoweb.course.services.exceptions.ResourceNotFoundExcepetion;
+
+import jakarta.servlet.http.HttpServletRequest;
+
+@ControllerAdvice // vai entercipar as expcetion que acontecerem para que eu consiga fazer o tratamento
+public class ResourceExcepetionHandler {
+	
+	@ExceptionHandler(ResourceNotFoundExcepetion.class) // capaz de entercipa para poder cair aqui dentro  
+	public ResponseEntity<StandarError> resourceNotFound(ResourceNotFoundExcepetion e, HttpServletRequest request ){
+		String error = "Resource not found";
+		HttpStatus status = HttpStatus.NOT_FOUND;
+		StandarError err = new StandarError(Instant.now(), status.value(), error, e.getMessage(), request.getRequestURI());
+		return ResponseEntity.status(status).body(err);
+	}
+}
---
+++ src/main/java/com/educandoweb/course/services/exceptions/ResourceNotFoundExcepetion.java
@@ -0,0 +1,10 @@
+package com.educandoweb.course.services.exceptions;
+
+public class ResourceNotFoundExcepetion extends RuntimeException {
+	private static final long serialVersionUID = 1L;
+	
+	public ResourceNotFoundExcepetion(Object id) {
+		super("Resource not found. Id "+id);
+	}
+	
+}
